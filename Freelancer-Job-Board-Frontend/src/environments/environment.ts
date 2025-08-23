2a6b1d242ed4d83034f2e319b4d2c7aaa8ad2de7
Supun-Sri <supunwickrama6@gmail.com>
2025-08-23 15:36:42 +0530

    https

--- Freelancer-Job-Board-Frontend/src/environments/environment.ts
+++ Freelancer-Job-Board-Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://freelance-job-board-with-mean-stack-ten.vercel.app/api',
+    apiUrl: 'https://freelance-job-board-with-mean-stack-ten.vercel.app/api',
 };
